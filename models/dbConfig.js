aee099675474ad569cf6597b2b12dbcf3a2b0df9
MrSociety404 <cstfabrice@gmail.com>
2021-05-19 22:45:25 +0200

    DB change

--- models/dbConfig.js
+++ models/dbConfig.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose.connect(
-  "mongodb://localhost:27017/cardealer",
+  "mongodb://45.9.191.251:27017/cardealer",
   {useNewUrlParser: true, useUnifiedTopology: true},
   (err) => {
     if(!err) console.log('MongoDB connected')
