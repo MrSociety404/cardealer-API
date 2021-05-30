c9728c3698a0cee7fc754cefb815a4f20259da44
MrSociety404 <cstfabrice@gmail.com>
2021-05-30 14:46:36 +0200

    auth

--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken')
 module.exports = async (req, res, next) => {
     
   try {
-    const token = req.headers.authorization.split(" ")[1]
+    const token = req.headers['X-token']
     const decoded = await jwt.verify(token, process.env.TOKEN_SECRET_KEY)
     res.user = decoded;
     next()
